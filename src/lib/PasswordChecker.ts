ef34524490b375a15dd9f693b29567aa8db0454d
andrewrunner <ant.tower97@gmail.com>
2023-12-11 20:31:02 +0300

    tdd PasswordCheker

--- src/lib/PasswordChecker.ts
+++ src/lib/PasswordChecker.ts
@@ -1,7 +1,59 @@
 
+export enum PasswordErrors {
+    SHORT = "Password must be more 8 or more characters length!",
+    NO_UPPER_CASE = "Password must have upper case letters!",
+    NO_LOWER_CASE = "Password must have lower case letters!",
+    NO_NUMBER = "Password must have numbers!",
+}
+
+export interface PasswordCheckResult {
+    isValid: boolean,
+    reasons: PasswordErrors[]
+}
+
 export class PasswordChecker {
 
-    public checkPassword() {
+    public checkPassword(password:string):PasswordCheckResult {
+        const checkErrors:PasswordErrors[] = [];
+
+        this.checkForLength(password, checkErrors);
+        this.checkForUpperCase(password, checkErrors);
+       this.checkForLowerCase(password, checkErrors);
         
+        return {
+            isValid: !checkErrors.length,
+            reasons: checkErrors
+        };
+    }
+
+    public checkAdminPassword(password:string):PasswordCheckResult { 
+        const basicCheck = this.checkPassword(password);
+        this.checkForNumber(password, basicCheck.reasons);
+        return basicCheck;
+    }
+
+
+
+
+    private checkForNumber(password: string, checkErrorsRef:PasswordErrors[]) {
+        const hasNumber = /\d/;
+        if(!hasNumber.test(password)) {
+            checkErrorsRef.push(PasswordErrors.NO_NUMBER);
+        }
+    }
+    private checkForLength(password: string, checkErrorsRef:PasswordErrors[]) {
+        if(password.length < 8) {
+            checkErrorsRef.push(PasswordErrors.SHORT);
+        }
+    }
+    private checkForUpperCase(password: string, checkErrorsRef:PasswordErrors[]) {
+       if(password == password.toLowerCase()) {
+           checkErrorsRef.push(PasswordErrors.NO_UPPER_CASE);
+        }
+    }
+    private checkForLowerCase(password: string, checkErrorsRef:PasswordErrors[]) {
+        if(password == password.toUpperCase()) {
+            checkErrorsRef.push(PasswordErrors.NO_LOWER_CASE);
+        }
     }
 }
--- test/tdd-examples/PasswordChecker.test.ts
+++ test/tdd-examples/PasswordChecker.test.ts
@@ -1,4 +1,4 @@
-import { PasswordChecker } from "../../src/lib/PasswordChecker"
+import { PasswordChecker, PasswordErrors } from "../../src/lib/PasswordChecker"
 
 describe('PasswordChecker test suite', () => {
 
@@ -9,9 +9,54 @@ describe('PasswordChecker test suite', () => {
     })
 
 
-    it('Do nothing', () => {
-        sut.checkPassword();
+    it('Password with less 8 charactest is invalid', () => {
+         let actual = sut.checkPassword('12345Ab');
+         expect(actual.isValid).toBe(false);
+         expect(actual.reasons).toContain(PasswordErrors.SHORT);
+    })
+    it('Password with more than 8 charactest is ok', () => {
+        let actual = sut.checkPassword('1234567Vd');
+        expect(actual.reasons).not.toContain(PasswordErrors.SHORT);
+   })
+
+
+   it('Password with no upper case letter is invalid', () => {
+        let actual = sut.checkPassword('abc');
+        expect(actual.isValid).toBe(false);
+        expect(actual.reasons).toContain(PasswordErrors.NO_UPPER_CASE);
+    })
+    it('Password with upper case letter is valid', () => {
+        let actual = sut.checkPassword('aBc');
+        expect(actual.reasons).not.toContain(PasswordErrors.NO_UPPER_CASE);
     })
 
+
+    it('Password with no lower case letter is invalid', () => {
+        let actual = sut.checkPassword('ABC12345678');
+        expect(actual.isValid).toBe(false);
+        expect(actual.reasons).toContain(PasswordErrors.NO_LOWER_CASE);
+    })
+    it('Password with lower case letter is valid', () => {
+        let actual = sut.checkPassword('aBc12345678');
+        expect(actual.reasons).not.toContain(PasswordErrors.NO_LOWER_CASE);
+    })
+
+
+    it('Valid password', () => {
+        let actual = sut.checkPassword('1234aBc567');
+        expect(actual.isValid).toEqual(true);
+        expect(actual.reasons).toHaveLength(0);
+    })
+
+
+
+    it('Admin password without number is invalid', () => {
+        let actual = sut.checkAdminPassword('ABCabc');
+        expect(actual.reasons).toContain(PasswordErrors.NO_NUMBER);
+    })
+    it('Admin password with number is valid', () => {
+        let actual = sut.checkAdminPassword('ABCabc1234');
+        expect(actual.reasons).not.toContain(PasswordErrors.NO_NUMBER);
+    })
 
 })
